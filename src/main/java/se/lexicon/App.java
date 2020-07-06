8d8b521500091c40cb83160099606d5d4879c9d7
Erik Svensson <eosvensson@gmail.com>
2020-07-06 10:26:37 +0200

    Xml IOC for AppUserDAO

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,9 +1,22 @@
 package se.lexicon;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import se.lexicon.data.AppUserDAO;
+import se.lexicon.model.AppUser;
+
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //1: Explain how to create bean with XML
+        //2: Try fetching the bean
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        AppUserDAO appUserDAO = context.getBean(AppUserDAO.class);
+
+        System.out.println(appUserDAO.save(new AppUser(null, "test@test.com", "1234", "Testy", "Test address")));
+
+        context.close();
     }
 }
